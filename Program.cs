88d7fbdf57a4fdde99dc8032722814badd38c274
naman-weybee <naman@weybee.in>
2023-02-08 13:29:43 +0530

    changes done

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace ArtValorem_Crawling
     class Program
     {
         private const string Url = "http://www.artvalorem.fr/recherche?language=fr&query=&isvisible=&myGroup=1&advancedRecherche=on&actuDatefilter=&venteType=all&ordre=score&categorie=9";
+        private const string BaseUrl = "http://www.artvalorem.fr";
 
         static void Main(string[] args)
         {
@@ -67,30 +68,117 @@ namespace ArtValorem_Crawling
             opt.AddArguments("--disable-notifications");
 
             ChromeDriver driver = new(ChromeDriverService.CreateDefaultService(), opt, TimeSpan.FromMinutes(3));
-            driver.Navigate().GoToUrl(Url);
-            Thread.Sleep(3000);
-
             try
             {
-                driver.FindElement(By.XPath(XpathStrings.AcceptCoockiesXpath)).Click();
-                GetFullyLoadedWebPage(driver);
-            }
-            catch (Exception) { Thread.Sleep(3000); }
+                driver.Navigate().GoToUrl(Url);
+                Thread.Sleep(3000);
 
-            try
-            {
-                int count = 2;
-                do
+                try
                 {
-                    NextPageControl(driver, count);
-                    count++;
-                } while (true);
-            }
-            catch (Exception) { Thread.Sleep(3000); }
+                    driver.FindElement(By.XPath(XpathStrings.AcceptCoockiesXpath)).Click();
+                    GetFullyLoadedWebPage(driver);
+                }
+                catch (Exception) { Thread.Sleep(3000); }
 
-            var pageSource = GetFullyLoadedWebPageContent(driver);
-            var Details = new HtmlDocument();
-            Details.LoadHtml(pageSource);
+                try
+                {
+                    int count = 2;
+                    do
+                    {
+                        var pageSource = GetFullyLoadedWebPageContent(driver);
+                        var Details = new HtmlDocument();
+                        Details.LoadHtml(pageSource);
+
+                        var AllAuctionList = Details.DocumentNode.SelectNodes(XpathStrings.AuctionListXpath);
+                        if (AllAuctionList != null)
+                        {
+                            foreach (var auction in AllAuctionList)
+                            {
+                                var auctionImageUrl = string.Empty;
+                                var auctionTitle = string.Empty;
+                                var auctionDescriptionString = string.Empty;
+                                var auctionDescription = string.Empty;
+                                var lotNumber = string.Empty;
+                                var estimationPriceString = string.Empty;
+                                var estimationPriceStart = string.Empty;
+                                var estimationPriceEnd = string.Empty;
+                                var estimationPriceCurrency = string.Empty;
+                                var resultPriceString = string.Empty;
+                                var resultPrice = string.Empty;
+                                var resultPriceCurrency = string.Empty;
+                                var saleOfDateString = string.Empty;
+                                var saleStartMonthString = string.Empty;
+                                var saleStartDate = string.Empty;
+                                var saleStartMonth = string.Empty;
+                                var saleStartYear = string.Empty;
+                                var auctionLink = string.Empty;
+                                var auctionId = string.Empty;
+
+                                auctionImageUrl = auction.SelectSingleNode(XpathStrings.AuctionImageUrlXpath)?.GetAttributes("src").First().Value.Replace("amp;", "") ?? string.Empty;
+                                auctionTitle = auction.SelectSingleNode(XpathStrings.AuctionTitleXpath)?.InnerText.Replace("\n", "").Replace("\r", "").Replace("&nbsp", "").Trim() ?? string.Empty;
+                                auctionDescriptionString = auction.SelectSingleNode(XpathStrings.AuctionDescriptionXpath)?.InnerHtml.Replace("\n", "").Replace("\r", "").Replace("&nbsp", "").Trim() ?? string.Empty;
+                                lotNumber = auction.SelectSingleNode(XpathStrings.LotCountXpath)?.InnerText.Replace("\n", "").Replace("\r", "").Replace("&nbsp", "").Trim() ?? string.Empty;
+                                estimationPriceString = auction.SelectSingleNode(XpathStrings.EstimationPriceXpath)?.InnerText.Replace("\n", "").Replace("\r", "").Replace("&nbsp", "").Trim() ?? string.Empty;
+                                resultPriceString = auction.SelectSingleNode(XpathStrings.ResultPriceXpath)?.InnerText.Replace("\n", "").Replace("\r", "").Replace("&nbsp", "").Trim() ?? string.Empty;
+                                saleOfDateString = auction.SelectSingleNode(XpathStrings.SaleOfDateXpath)?.InnerText.Replace("\n", "").Replace("\r", "").Replace("&nbsp", "").Trim() ?? string.Empty;
+                                auctionLink = BaseUrl + auction.SelectSingleNode(XpathStrings.AuctionLinkXpath)?.GetAttributes("href").First().Value ?? string.Empty;
+
+                                if (!string.IsNullOrEmpty(auctionDescriptionString))
+                                    auctionDescription = RegexString.AuctionDescriptionRegex.Match(auctionDescriptionString).Groups[1].Value.Trim() ?? string.Empty;
+
+                                if (!string.IsNullOrEmpty(estimationPriceString))
+                                {
+                                    estimationPriceStart = RegexString.EstimationPriceRegex.Match(estimationPriceString).Groups[1].Value ?? string.Empty;
+                                    estimationPriceEnd = RegexString.EstimationPriceRegex.Match(estimationPriceString).Groups[3].Value ?? string.Empty;
+                                    estimationPriceCurrency = RegexString.EstimationPriceRegex.Match(estimationPriceString).Groups[5].Value ?? string.Empty;
+                                }
+
+                                if (!string.IsNullOrEmpty(resultPriceString))
+                                {
+                                    resultPrice = RegexString.ResultPriceRegex.Match(resultPriceString).Groups[1].Value ?? string.Empty;
+                                    resultPriceCurrency = RegexString.ResultPriceRegex.Match(resultPriceString).Groups[3].Value ?? string.Empty;
+                                }
+
+                                if (!string.IsNullOrEmpty(saleOfDateString))
+                                {
+                                    saleStartDate = RegexString.SaleOfDateRegex.Match(saleOfDateString).Groups[1].Value ?? string.Empty;
+                                    saleStartMonthString = RegexString.SaleOfDateRegex.Match(saleOfDateString).Groups[3].Value ?? string.Empty;
+                                    saleStartYear = RegexString.SaleOfDateRegex.Match(saleOfDateString).Groups[5].Value ?? string.Empty;
+
+                                    if (!string.IsNullOrEmpty(saleStartMonthString))
+                                        saleStartMonth = Enum.GetName(typeof(NumberToMonth), Convert.ToInt32(saleStartMonthString)) ?? string.Empty;
+                                }
+
+                                if (!string.IsNullOrEmpty(auctionLink))
+                                    auctionId = RegexString.AuctionIdRegex.Match(auctionLink).Groups[1].Value ?? string.Empty;
+
+                                Console.WriteLine($"----------Auction with Id = {auctionId}----------");
+                                Console.WriteLine($"Auction Title: {auctionTitle}");
+                                Console.WriteLine($"Auction Image Url: {auctionImageUrl}");
+                                Console.WriteLine($"Auction Description: {auctionDescription}");
+                                Console.WriteLine($"Auction Link: {auctionLink}");
+                                Console.WriteLine($"Lot Number: {lotNumber}");
+                                Console.WriteLine($"Estimation Price Start: {estimationPriceStart}");
+                                Console.WriteLine($"Estimation Price End: {estimationPriceEnd}");
+                                Console.WriteLine($"Estimation Price Currency: {estimationPriceCurrency}");
+                                Console.WriteLine($"Result Price: {resultPrice}");
+                                Console.WriteLine($"Result Price Currency: {resultPriceCurrency}");
+                                Console.WriteLine($"Sale Start Date: {saleStartDate}");
+                                Console.WriteLine($"Sale Start Month: {saleStartMonth}");
+                                Console.WriteLine($"Sale Start Year: {saleStartYear}");
+                                Console.WriteLine();
+                            }
+                        }
+                        NextPageControl(driver, count);
+                        count++;
+                    } while (true);
+                }
+                catch (Exception) { Thread.Sleep(3000); }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
 
         public static void NextPageControl(ChromeDriver driver, int count)
--- Service/RegexString.cs
+++ Service/RegexString.cs
@@ -9,18 +9,26 @@ namespace Phillips_Crawling_Task.Service
 {
     public class RegexString
     {
-        public static readonly Regex TimeDurationRegex = new(@"(\d+)(\s?\,?\-?)(\w+)(\s?\,?\-?)(\d{4})(\s?\,?\-?)(\d+\:\d+\s\w+)(\s+?\,?\-?)([^\n\r])(\w+)", RegexOptions.IgnoreCase);
-        public static readonly Regex SaleNumberRegex = new(@"(\d+)", RegexOptions.IgnoreCase);
-        public static readonly Regex AuctionDateRegex = new(@"(\w+)(\s?\,?\-?)(\d+)(\s?\,?\-?\s?)(\w+)(\s?\,?\-?)(\d+)(\s?\,?\-?\s?)(\d{4})", RegexOptions.IgnoreCase);
-        public static readonly Regex DimensionRegex = new(@"(\d+.\d+)(\s?)(\w+)(\s?\w?\s?)(\d+.\d+)(\s?)(\w+)", RegexOptions.IgnoreCase);
-        public static readonly Regex AuctionIdRegex = new(@"Auction=([^\n\r]*)", RegexOptions.IgnoreCase);
-        public static readonly Regex ReferenceNoRegex = new(@"Reference: (.*)<br>", RegexOptions.IgnoreCase);
-        public static readonly Regex CaseMaterialRegex = new(@"Case Material: (.*)<br>", RegexOptions.IgnoreCase);
-        public static readonly Regex ConditionRegex = new(@"Condition: (.*)", RegexOptions.IgnoreCase);
-        public static readonly Regex CircaRegex = new(@"Circa: (.*)<br>", RegexOptions.IgnoreCase);
-        public static readonly Regex WinningBidRegex = new(@"^(\w+)?(\s?)(\w+)?(\s?\:?\s?)(\D*)(\s)(\d+)(\D*)$", RegexOptions.IgnoreCase);
-        public static readonly Regex EstimateRegex = new(@"^(\w+)?(\:?\s?)(\D+)(\s)(\d+)(\s?\-?\s?)(\d+)", RegexOptions.IgnoreCase);
-        public static readonly Regex CaseDiameterRegex = new(@"Case Diameter: (.*)<br>", RegexOptions.IgnoreCase);
-        public static readonly Regex CaseDimensionRegex = new(@"(\d+.?\d+)?(\s?)(\w+)?(\s?\w?\s?)(\d+.?\d+)?(\s?)(\w+)?", RegexOptions.IgnoreCase);
+        public static readonly Regex AuctionDescriptionRegex = new(@"</div>(.*)<br><br>", RegexOptions.IgnoreCase);
+        public static readonly Regex EstimationPriceRegex = new(@"(\d+)(\s?\-?\s?)(\d+)?(\s?\-?\s?)(\w+)?", RegexOptions.IgnoreCase);
+        public static readonly Regex ResultPriceRegex = new(@"(\d+)(\s?\-?\s?)(\w+)?", RegexOptions.IgnoreCase);
+        public static readonly Regex SaleOfDateRegex = new(@"(\d+)?(\s?\-?\s?)(\d+)?(\s?\-?\s?)(\d{4})", RegexOptions.IgnoreCase);
+        public static readonly Regex AuctionIdRegex = new(@"(\d+)", RegexOptions.IgnoreCase);
+    }
+
+    public enum NumberToMonth
+    {
+        January = 1,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December,
     }
 }
